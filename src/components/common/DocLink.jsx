d0478a3038e819a3d8efb911d653d8e69c2db0fb
ahmed rakan <ar.aldhafeeri11@gmail.com>
2024-05-24 06:08:06 +0300

    fix router github pages

--- src/components/common/DocLink.jsx
+++ src/components/common/DocLink.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function DocLink({link, text, icon}) {
   return (
-    <a href={link} className='flex flex-row mt-2 mb-2 bg-[#0D1117] text-sm border border-[#00df9a] rounded-lg ml-1 w-[125px] h-[20px] items-center font-medium rtl:text-right  hover:scale-105'>
+    <Link to={{pathname: link}} className='flex flex-row mt-2 mb-2 bg-[#0D1117] text-sm border border-[#00df9a] rounded-lg ml-1 w-[125px] h-[20px] items-center font-medium rtl:text-right  hover:scale-105'>
       <span className='ml-1'>{icon}</span>
       <span className='text-[#00df9a] ml-2 '>{text}</span>
-    </a>
+    </Link>
   )
 }
